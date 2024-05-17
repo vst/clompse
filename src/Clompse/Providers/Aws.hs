8a6054501ee21c2ee0de821586ba6f64c91b7185
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2024-05-17 08:44:09 +0800

    feat: list DNS records managed on AWS Lightsail

--- src/Clompse/Providers/Aws.hs
+++ src/Clompse/Providers/Aws.hs
@@ -5,6 +5,7 @@ module Clompse.Providers.Aws (
   AwsConnection (..),
   listBucketsLightsail,
   listBucketsS3,
+  listDnsRecordsLightsail,
   listDnsRecordsRoute53,
   listDomainsLightsail,
   listDomainsRoute53,
@@ -13,7 +14,7 @@ module Clompse.Providers.Aws (
 ) where
 
 import Clompse.Providers.Aws.ApiAws (listBucketsS3, listDnsRecordsRoute53, listDomainsRoute53, listServersEc2)
-import Clompse.Providers.Aws.ApiLightsail (listBucketsLightsail, listDomainsLightsail, listServersLightsail)
+import Clompse.Providers.Aws.ApiLightsail (listBucketsLightsail, listDnsRecordsLightsail, listDomainsLightsail, listServersLightsail)
 import Clompse.Providers.Aws.Connection (AwsConnection (..))
 import Clompse.Providers.Aws.Error (AwsError (..))
 
